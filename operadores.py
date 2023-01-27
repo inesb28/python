3a9080a5ea2d042186ed21ab99c3d4cb0b7a3de2
unknown <2btd04@iesramonllull.local>
2023-01-27 09:34:25 +0100

    if y elif's

---
+++ operadores.py
@@ -0,0 +1,17 @@
+a = 3
+b = 4
+
+if a == b:
+    print("son iguales")
+elif a != b:
+    print("no son igules")
+
+if a > b:
+    print("a es mayor que b")
+
+elif a < b:
+    print("b es mayor que a")
+
+a > b # False
+a < b # True
+
