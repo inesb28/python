cc7a09c8d69e06f8e176c6953fe2ead7d7a0f526
unknown <2btd04@iesramonllull.local>
2023-01-27 09:25:06 +0100

    (iniciles)

---
+++ nombre.py
@@ -0,0 +1,5 @@
+nombre = input("hola, me podrías decir cual es tu nombre? ")
+apellido1 = input("cual es tu primer apellido? ")
+apellido2 = input("y tu segundo apellido? ")
+
+print("hola! tus iniciales son "+nombre[0]+apellido1[0]+apellido2[0])
