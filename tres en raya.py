d0529ba887898b7114662b046c97da2a225fc3a8
unknown <2btd04@iesramonllull.local>
2023-02-06 09:01:58 +0100

    finish 3 en raya

--- tres en raya.py
+++ tres en raya.py
@@ -1,4 +1,7 @@
-casillas = ["","","","","","","","","",]
+import random
+
+casillas = [" "," "," "," "," "," "," "," "," ",]
+casillas_libres = [0,1,2,3,4,5,6,7,8]
 
 you = input("Quieres ser cara o cruz? ")
 if you == "x":
@@ -6,49 +9,73 @@ if you == "x":
 if you == "o":
     pc = "x"
 
-    while fin="no":
+final = False
+
+while not final:
+
+    if casillas_libres == []:
+        print("Empate!")
+        final = True
 
     posicion = int(input("Elige una casilla del 0 al 8: "))
     casillas[posicion] = you
+    casillas_libres.remove(posicion)
 
     if casillas[0] == casillas [1] == casillas [2] == you:
         print("has ganado!")
+        final = True
     if casillas[3] == casillas [4] == casillas [5] == you:
         print("has ganado!")
+        final = True
     if casillas[6] == casillas [7] == casillas [8] == you:
         print("has ganado!")
+        final = True
     if casillas[0] == casillas [3] == casillas [6] == you:
         print("has ganado!")
+        final = True
     if casillas[1] == casillas [4] == casillas [7] == you:
         print("has ganado!")
+        final = True
     if casillas[2] == casillas [5] == casillas [8] == you:
         print("has ganado!")
+        final = True
     if casillas[0] == casillas [4] == casillas [8] == you:
         print("has ganado!")
+        final = True
     if casillas[2] == casillas [4] == casillas [6] == you:
         print("has ganado!")
+        final = True
 
-    pc = ranom.randit(0,8)
+    if not casillas_libres == []:
+        posicion = random.choice(casillas_libres)
+        casillas[posicion] = pc
+        casillas_libres.remove(posicion)
 
     if casillas[0] == casillas [1] == casillas [2] == pc:
         print("has perdido!")
+        final= True
     if casillas[3] == casillas [4] == casillas [5] == pc:
         print("has perdido!")
+        final = True
     if casillas[6] == casillas [7] == casillas [8] == pc:
         print("has perdido!")
+        final = True
     if casillas[0] == casillas [3] == casillas [6] == pc:
         print("has perdido!")
+        final = True
     if casillas[1] == casillas [4] == casillas [7] == pc:
         print("has perdido!")
+        final = True
     if casillas[2] == casillas [5] == casillas [8] == pc:
         print("has perdido!")
+        final = True
     if casillas[0] == casillas [4] == casillas [8] == pc:
         print("has perdido!")
+        final = True
     if casillas[2] == casillas [4] == casillas [6] == pc:
         print("has perdido!")
+        final = True
 
     print("| "+casillas[0]+" | "+casillas[1]+" | "+casillas[2]+" |")
     print("| "+casillas[3]+" | "+casillas[4]+" | "+casillas[5]+" |")
     print("| "+casillas[6]+" | "+casillas[7]+" | "+casillas[8]+" |")
-
-    fin = input("ha terminado la partida?")
