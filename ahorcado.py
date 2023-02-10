ccfafc6b4fbf8faa353dd51b9b4d47d307a85119
unknown <2btd04@iesramonllull.local>
2023-02-10 09:06:00 +0100

    terminar ahorcado

--- ahorcado.py
+++ ahorcado.py
@@ -1,19 +1,26 @@
-word = ("azul")
+import random
+
+words = ["poema","software","contento","hierva","periscopio","cenizas","azul","castigado","sentir","fotocopia"]
+
+word = random.choice(words)
 
 letras_check = []
 letras_not = []
 
-while True:
+play = True
+
+while play:
 
     for letra in word:
         if letra in letras_check:
-            print(letra,end=" ")
+            print(letra,end="")
         else:
-            print("_",end=" ")
+            print("_ ",end="")
 
     letra_ask = input("\ndime una letra: \n")
-
+    
     print(letra_ask in word)
+
     if letra_ask in word:
         letras_check.append(letra_ask)
     else:
@@ -21,3 +28,11 @@ while True:
 
     print("correctas: ",letras_check)
     print("incorrectas: ",letras_not)
+    
+    if set(letras_check) == set(word):
+        play = False
+        print("you win!")
+    if len(letras_not) == 12:
+        play = False
+        print("has perdido!")
+        print("la palabra secreta era ",word)
