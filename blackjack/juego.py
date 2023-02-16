4ae6caa380bdf469be7da2a7fbf1e7fb75cbe44a
unknown <2btd04@IESRAMONLLULL.LOCAL>
2023-02-16 13:06:27 +0100

    bj a medias

--- blackjack/juego.py
+++ blackjack/juego.py
@@ -5,7 +5,9 @@ mibaraja = Baraja()
 mano1 = Mano()
 mibaraja.mezclar()
 
-input("buenos días, vamos a jugar al 21")
+print("buenos días, vamos a jugar al 21, empezarmos con dos cartas")
+
+print("tus dos primeras cartas son:")
 
 carta_cogida = mibaraja.coger_carta()
 mano1.añadir_carta(carta_cogida)
@@ -14,4 +16,30 @@ mano1.añadir_carta(carta_cogida)
 mano1.mostrar_mano()
 
 mano1.calcular_valor()
-print(mano1.valor)
+
+juego = True
+
+if mano1.valor == 21:
+    print("Enhorabuena, has ganado!!")
+    juego = False
+
+while juego == True:
+    print("tu puntuación es de ",mano1.valor,)
+    ok = input("seguimos jugando? ")
+    
+    if not ok == "ok":
+        print("ok,bye!")
+        juego = False
+
+    if ok == "ok":
+        carta_cogida = mibaraja.coger_carta()
+        mano1.añadir_carta(carta_cogida)
+        mano1.mostrar_mano()
+
+        mano1.calcular_valor()
+
+        juego = True
+    
+    if mano1.valor > 21:
+        print("oh, no! te has pasado")
+        juego = False
